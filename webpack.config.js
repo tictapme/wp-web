606207d7cf0029b03b62762ecf87389eb61af7bb
GuilleF <guilleferrer@gmail.com>
2022-03-18 10:34:46 +0100

    Moves all web to src/ folder.

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+console.log('dirname : ', __dirname)
+module.exports = {
+    context: __dirname,
+    entry: './functions/index.ts',
+    output: {
+        filename: 'worker.js',
+        path: path.join(__dirname, 'dist'),
+    },
+    devtool: 'source-map',
+    mode: 'development',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                options: {
+                    // transpileOnly is useful to skip typescript checks occasionally:
+                     transpileOnly: true,
+                },
+            },
+        ],
+    },
+}
