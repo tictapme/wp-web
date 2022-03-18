3c387d296280947eac10c1cc6b907d13a7a2ecd7
GuilleF <guilleferrer@gmail.com>
2022-03-18 10:57:29 +0100

    Removes buggy contact-form.js

--- functions/contact-form.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestPost() {
-  // ...
-  return new Response(`Hello world`);
-}
-export async function handleRequest(request: Request): Promise<Response> {
-  return new Response("Hello worker!", {
-    headers: { "content-type": "text/plain" },
-  });
-}
