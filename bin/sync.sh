ba43f41464b95ad87086c857ff86ae1dae5ab8fa
GuilleF <guilleferrer@gmail.com>
2022-07-14 09:55:10 +0200

    Updates wordpress to 6.0 and fixes sitemap issues

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rsync -avz --exclude='.git/' --exclude='404.html' --exclude='bin/' debian@51.83.111.98:/home/debian/docker-wp/generated/ src/ --delete
-node ..\scripts\add-absolute-url-sitemap.js
+node bin/add-absolute-url-sitemap.js
