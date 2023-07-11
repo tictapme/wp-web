85bd1810e9cb616037243c20561f1eb849580b87
GuilleF <guilleferrer@gmail.com>
2023-07-11 19:11:33 +0200

    Adds redirects

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -avz --exclude='.git/' --exclude='404.html' --exclude='bin/' debian@51.83.111.98:/home/debian/docker-wp/generated/ src/ --delete
+rsync -avz --exclude='.git/' --exclude='404.html' --exclude='bin/' --exclude='_redirects' debian@51.83.111.98:/home/debian/docker-wp/generated/ src/ --delete
 node bin/add-absolute-url-sitemap.js
